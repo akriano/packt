20d2150ad874d1161e876f7174ea14c2f4332b51
akriano <faouzilaloui@yahoo.com>
2017-10-31 15:39:18 +0100

    Update webworker.js

--- webworker.js
+++ webworker.js
@@ -1,10 +1,11 @@
 onmessage= function(e){
 if(e.data!==undefined){
-var fact=function (val){
+var fact=function foo(val){
 if(val<=1){
 return 1;
 
 }
+  return val*foo(val-1);
 //return val*argument.callee(val-1);
 }e.data;
 
