229986b12164d6def6f3744a57dbabbb82bc6bc2
akriano <faouzilaloui@yahoo.com>
2017-10-31 15:30:32 +0100

    Update Khena.js

--- Khena.js
+++ Khena.js
@@ -1,1 +1,21 @@
+var calculate = document.getElementById("calculate");
+var output =getElemntById("output");
 
+var operation=function(){
+if(window.worker){
+
+var worker=new Worker("webworker.js");
+var num= Number(document.getElementById("fact").value);
+worker.postMessage(num);
+woker.onmessage=function(e){
+output.innerHTML= e.data;
+};
+
+
+
+}
+
+}
+
+
+calculate.addEventListner("click",operation);
